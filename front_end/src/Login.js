8dc7d8e8d683789fd9e93a6b8d5b9c5f8459a52c
Ramón <ramonls2002@gmail.com>
2024-07-12 13:16:53 +0200

    server changes

--- front_end/src/Login.js
+++ front_end/src/Login.js
@@ -1,12 +1,33 @@
 import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client'
+
+const socket = io('http://localhost:4000')
 
 function Login()
 {
     //TO DO Add lobby functionality
     //TO DO Connect to JSON server (locally)
 
+    // TESTEO DE SERVER.JS
+    const [roomId, setRoomId] = useState(-1);
+
+    const send = (e) => {
+        e.preventDefault();
+        console.log("hola")
+        socket.emit('findRoom',)
+    };
+
+    socket.on('findRoom', roomNumber => {
+        setRoomId(roomNumber);
+    })
+
     return(
-        <p> Under Construction </p>
+        <div>
+            <button onClick={send}>
+                Send
+            </button>
+            <div> Room number: {roomId} </div>
+        </div>
     );
 }
 
