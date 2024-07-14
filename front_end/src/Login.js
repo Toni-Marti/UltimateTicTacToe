b796f6ebd4966c4268f6a9b9fa50636a8e40fa64
Aaron <dev.aargar26@gmail.com>
2024-07-14 18:05:46 +0900

    Update Login.js

--- front_end/src/Login.js
+++ front_end/src/Login.js
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client'
 import {Credentials, EVENTTYPE, SocketEvent} from './commonsSymbolicLink/socketUtils.js'
@@ -8,10 +9,36 @@ const socket = io('http://localhost:4000')
 function Login()
 {
     
+    const [users, setUsers] = useState([]);
+    const [message, setMessage] = useState('');
+    const [pw, setPw] = useState("");
+    const [userName, setUserName] = useState(""); 
 
+    const send = (e) => {
+        e.preventDefault();
+        socket.emit('login',{userName,pw})
+      };
+    
     return(
-        <div>
-            ...
+        <div className='Login'>
+            <h1>Sign-Up:</h1>
+            <form onSubmit={send} className="chat">
+                <input
+                    type="text"
+                    placeholder={"Username"}
+                    name="userName"
+                    value={userName}
+                    onChange={(e) => setUserName(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder={"Password"}
+                    name="msg"
+                    value={pw}
+                    onChange={(e) => setPw(e.target.value)}
+                />
+                <button type="submit">Login</button>
+            </form>
         </div>
     );
 }
